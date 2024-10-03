b0293258df7264c91cfd112a2e9d7ec7b82f93e0
MAIPA01 <marcelipatryk@gmail.com>
2024-10-03 23:05:22 +0200

    single file with necesery headers

---
+++ GLSL-Struct-Cpp/include/libraries.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <format>
+#include <functional>
+#include <queue>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
--- GLSL-Struct-Cpp/include/pch.h
+++ GLSL-Struct-Cpp/include/pch.h
@@ -9,13 +9,6 @@
 
 // w tym miejscu dodaj nagłówki, które mają być wstępnie kompilowane
 #include <framework.h>
-#include <string>
-#include <unordered_map>
-#include <vector>
-#include <format>
-#include <functional>
-#include <queue>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
+#include <libraries.h>
 
 #endif //PCH_H
